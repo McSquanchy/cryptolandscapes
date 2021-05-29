ba8854b9914fc94c5eecfc4f94cd7ce64e231ea8
Kevin Buman <kevin.buman@students.fhnw.ch>
2021-05-29 14:21:49 +0200

    added action to update a single nft

--- src/state/slices/myLandscapes.reducer.js
+++ src/state/slices/myLandscapes.reducer.js
@@ -11,6 +11,11 @@ export const myLandscapesSlice = createSlice({
             console.log("action is", landscapes);
             state.landscapes = landscapes;
         },
+        setMyLandscape: (state, {payload: { landscape } }) => {
+            console.log("action is", landscape);
+            const oldOne = state.landscapes.filter(el => el.id === landscape.id)[0];
+            state.landscapes[state.landscapes.indexOf(oldOne)] = landscape;
+        },
         finishMyLandscapesLoading: (state) => {
             state.loading = false;
         },
@@ -21,6 +26,6 @@ export const myLandscapesSlice = createSlice({
 });
 
 // Action creators are generated for each case reducer function
-export const { setMyLandscapes, finishMyLandscapesLoading, startMyLandscapesLoading } = myLandscapesSlice.actions;
+export const { setMyLandscape, setMyLandscapes, finishMyLandscapesLoading, startMyLandscapesLoading } = myLandscapesSlice.actions;
 
 export default myLandscapesSlice.reducer;
